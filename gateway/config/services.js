3bb6113ba5a4894abda4ca181cc4efd17db34673
chamathk77 <152027040+chamathk77@users.noreply.github.com>
2026-03-27 15:06:59 +0530

    Revert "intiall commite"
    This reverts commit cc390ef213185154c21e09a66e5ec1824ab27175.

--- gateway/config/services.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  user: process.env.USER_SERVICE_URL || 'http://localhost:3001',
-  menu: process.env.MENU_SERVICE_URL || 'http://localhost:3002',
-  order: process.env.ORDER_SERVICE_URL || 'http://localhost:3003',
-  payment: process.env.PAYMENT_SERVICE_URL || 'http://localhost:3004',
-};
